775e8c7f14a5393e2fcccc05b63e3710471c49d0
Dip Kumar Das <80997532+dip000das@users.noreply.github.com>
2024-01-10 20:43:29 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,8 @@ export const CDN_URL =
 export const LOGO_URL =
   'https://png.pngtree.com/png-vector/20230217/ourmid/pngtree-food-logo-design-for-restaurant-and-business-png-image_6604922.png';
 
-export const MENU_API =
-  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5148386&lng=88.4097881&restaurantId=';
+// export const MENU_API =
+//   'https://corsproxy.org/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5148386&lng=88.4097881&restaurantId=';
+
+
+export const MENU_API ='https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D28.6696613%26lng%3D77.0568756%26restaurantId%3D';
